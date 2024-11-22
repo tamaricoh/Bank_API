81e17c77726e0c7f57b0177df642c9cbfc247198
tamaricoh <106402042+tamaricoh@users.noreply.github.com>
2024-11-22 23:26:26 +0200

    Building a basic server for registration and login

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	api_sec "github.com/tamaricoh/F5_home_assignment/pkg" // Import your api_sec package
+)
 
 func main() {
-	
+	// Define your routes
+	http.HandleFunc("/register", api_sec.Register)                    // works
+	http.HandleFunc("/login", api_sec.Login)                          // works
+	http.Handle("/accounts", api_sec.Auth(api_sec.AccountsHandler))
+
+	// Start the server
+	log.Println("Starting server on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
 }
